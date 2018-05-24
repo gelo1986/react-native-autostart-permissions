37df47e0d00c9fa6d74525e2d50d131806e75077
TemaKozyrev <tema@tema.ws>
2018-05-24 13:14:29 +0300

    Update AutoStartPermissions.java

--- android/src/main/java/co/digitalwing/autostartpermissions/AutoStartPermissions.java
+++ android/src/main/java/co/digitalwing/autostartpermissions/AutoStartPermissions.java
@@ -39,6 +39,7 @@ public class AutoStartPermissions extends ReactContextBaseJavaModule {
 
     private static List<Intent> POWERMANAGER_INTENTS = new ArrayList<>(Arrays.asList(
             new Intent().setComponent(new ComponentName("com.miui.securitycenter", "com.miui.permcenter.autostart.AutoStartManagementActivity")),
+            new Intent().setComponent(new ComponentName("com.meizu.safe", "com.meizu.safe.powerui.PowerAppPermissionActivity")),
             new Intent().setComponent(new ComponentName("com.letv.android.letvsafe", "com.letv.android.letvsafe.AutobootManageActivity")),
             new Intent().setComponent(new ComponentName("com.huawei.systemmanager", "com.huawei.systemmanager.optimize.process.ProtectActivity")),
             new Intent().setComponent(new ComponentName("com.coloros.safecenter", "com.coloros.safecenter.permission.startup.StartupAppListActivity")),
